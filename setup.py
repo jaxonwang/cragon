9779305f85410ebc202daffce2fd3f45107bf75f
JX Wang <jxwang92@gmail.com>
2020-05-28 05:22:21 +0900

    force build dmtcp first

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import distutils
 import glob
 
 from setuptools import setup, Extension
-from distutils.command.build import build as build_orig
 from setuptools.command.build_ext import build_ext as build_ext_orig
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -13,65 +12,65 @@ DMTCP_PLUGIN_s = "dmtcp_plugin"
 DMTCP_PLUGIN_DIR = os.path.join(ROOT_DIR, DMTCP_PLUGIN_s)
 
 
-class DMTCPPlugin(Extension):
+def build_DMTCP_plugin(build_cmd):
+    cwd = os.getcwd()
+    os.chdir(DMTCP_PLUGIN_DIR)
+    # make
+    make_cmd = ["make"]
+    if build_cmd.debug:
+        make_cmd += ["debug"]
+    if build_cmd.parallel:
+        make_cmd += ["-j", str(build_cmd.parallel)]
+    build_cmd.spawn(make_cmd)
+
+    plugin_out_dir = "." if not build_cmd.debug else "debug"
+    files = glob.glob(os.path.join(plugin_out_dir, "*.so"))
+    files = [os.path.abspath(f) for f in files]
+
+    os.chdir(cwd)
+
+    # copy to build/lib../cragon/lib
+    dst = os.path.join(build_cmd.build_lib, "cragon", "lib")
+    if not os.path.isdir(dst):
+        os.mkdir(dst)
+    for f in files:
+        distutils.file_util.copy_file(f, dst)
+
+
+def build_DMTCP(build_cmd):
+    cwd = os.getcwd()
+    os.chdir(DMTCP_DIR)
+    # conf
+    if not os.path.isfile("Makefile"):  # TODO allow user to config
+        configure_shell = os.path.join(DMTCP_DIR, "configure")
+        build_cmd.spawn([configure_shell])
+    # make
+    make_cmd = ["make"]
+    if build_cmd.parallel:
+        make_cmd += ["-j", str(build_cmd.parallel)]
+    build_cmd.spawn(make_cmd)
+
+    os.chdir(cwd)
+
+    # copy to build/lib../lib
+    dmtcp_out_dirs = ["bin", "lib"]
+    for d in dmtcp_out_dirs:
+        distutils.dir_util.copy_tree(
+            os.path.join(
+                DMTCP_s, d), os.path.join(
+                build_cmd.build_lib, "cragon", d))
+
+
+class DMTCPandPlugin(Extension):
 
     def __init__(self, name):
         super().__init__(name, sources=[])
 
     @staticmethod
     def do_build(build_cmd):
-        cwd = os.getcwd()
-        os.chdir(DMTCP_PLUGIN_DIR)
-        # make
-        make_cmd = ["make"]
-        if build_cmd.debug:
-            make_cmd += ["debug"]
-        if build_cmd.parallel:
-            make_cmd += ["-j", str(build_cmd.parallel)]
-        build_cmd.spawn(make_cmd)
-
-        plugin_out_dir = "." if not build_cmd.debug else "debug"
-        files = glob.glob(os.path.join(plugin_out_dir, "*.so"))
-        files = [os.path.abspath(f) for f in files]
-
-        os.chdir(cwd)
-
-        # copy to build/lib../cragon/lib
-        dst = os.path.join(build_cmd.build_lib, "cragon", "lib")
-        if not os.path.isdir(dst):
-            os.mkdir(dst)
-        for f in files:
-            distutils.file_util.copy_file(f, dst)
-
-
-class DMTCP(Extension):
-
-    def __init__(self, name):
-        super().__init__(name, sources=[])
-
-    @staticmethod
-    def do_build(build_cmd):
-        cwd = os.getcwd()
-        os.chdir(DMTCP_DIR)
-        # conf
-        if not os.path.isfile("Makefile"):  # TODO allow user to config
-            configure_shell = os.path.join(DMTCP_DIR, "configure")
-            build_cmd.spawn([configure_shell])
-        # make
-        make_cmd = ["make"]
-        if build_cmd.parallel:
-            make_cmd += ["-j", str(build_cmd.parallel)]
-        build_cmd.spawn(make_cmd)
-
-        os.chdir(cwd)
-
-        # copy to build/lib../lib
-        dmtcp_out_dirs = ["bin", "lib"]
-        for d in dmtcp_out_dirs:
-            distutils.dir_util.copy_tree(
-                os.path.join(
-                    DMTCP_s, d), os.path.join(
-                    build_cmd.build_lib, "cragon", d))
+        # should build in this order
+        build_DMTCP(build_cmd)
+        build_DMTCP_plugin(build_cmd)
 
 
 class build_ext(build_ext_orig):
@@ -90,7 +89,7 @@ setup(
     author_email="jxwang92@gmail.com",
     install_requires=["Click"],
     packages=['cragon'],
-    ext_modules=[DMTCP("dmtcp"), DMTCPPlugin("dmtcp_plugin")],
+    ext_modules=[DMTCPandPlugin("all")],
     cmdclass={
         'build_ext': build_ext,
     },
